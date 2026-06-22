e046614954bfdc7aabecb26bd5279e21c8b9b95e
sana-mulla789 <hussainbe6852@gmail.com>
2026-06-22 15:30:09 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Prince and yogesh , jay from batch-43!");
+        System.out.println("helloo sana");
     }
 }
